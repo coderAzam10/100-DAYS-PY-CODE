329287037f98a5586d76ebe56525cd432c89af7a
coderAzam10 <asazamshaikh@gmail.com>
2026-09-15 22:59:10 +0530

    Day 1 complete - Band Name Generator

---
+++ Day_01.py
@@ -0,0 +1,49 @@
+
+#print("Hello, World!")
+
+
+#print("Hello, World!\n Hello, World!\nHello, World!\nHello, World!\nHello, World!\n ")
+
+
+#print("hello " + "Shaikh")
+
+#input fxn 
+
+#input("What is your name? ")
+# input fxn ko print ke sath use krke hum bhut kuchh input daal ke print kr skte hain 
+
+# print("Hello "  + input("what is your name?!"))
+ 
+#variables
+
+#name = "azam shaikh"
+#print(len(name))
+
+# name = "azam"
+# print(name)
+# name = "jack"
+
+
+# (print(len(input("what is your name?!"))))
+# also we can use this too
+
+# username = input("what is your name?!")
+# length = len(username)  
+# print(length) 
+
+# Band name Generator
+print("Welcome to the band Name Generator.")
+
+city = input("What is the name of the city you grew up in ?\n")
+pet = input("What is the name of your pet ?\n")
+
+print("Your band name could be " + city + " " + pet)
+
+
+
+
+
+
+
+
+
